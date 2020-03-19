53b8cb0b9ad8da11e5725ad4d9c672fa60ab276d
JoeA42 <36778393+joea42@users.noreply.github.com>
2020-03-19 11:23:33 -0600

    This is a commit and push I made using the IDE

--- Git Test/src/Main/Main.java
+++ Git Test/src/Main/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("this is a test");
         System.out.println("this is another test");
+        System.out.println("this is a third test");
     }
 }
